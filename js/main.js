e2c2ed6ab907e023bcd71daca44e9dbea52b6668
red0r4nge <a.kurilenko00@gmail.com>
2022-10-11 19:26:07 +0300

    changed .container__backgound to .container .container--special in index, team. Deleted slash for input #info__toggle

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let block = document.getElementById('menu');
+let menu = document.getElementById('menu');
 let close = document.getElementById('close');
 
 console.log(menu);
@@ -6,4 +6,23 @@ console.log(close);
 
 close.addEventListener('click', function () {
   menu.style.display = "none";
-});
+})
+
+
+//Если меню disp block, то при нажатии на close menu display: none/ Else menu display: none, то при клике на бургер должен быть display block для меню.
+
+// function closeButton() {
+//   let menu = document.getElementById('menu');
+//   let close = document.getElementById('close');
+
+//   console.log(menu);
+//   console.log(close);
+
+//   if (menu.style.display = "block") {
+//     close.addEventListener('click', function () {
+//       menu.style.display = "none";
+//     });
+//   }
+
+//   else (menu.style.display = "none")
+// }
